99b5afb00f2c0e3f09e71639e5acc90e7f4a6a52
Gunnar Völkel <Gunnar.Voelkel@uni-ulm.de>
2020-04-22 10:57:13 +0200

    Add Leiningen config.

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject org.clojars.guv/darkstar "0.1.0"
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.graalvm.js/js "20.0.0"]
+                 [org.graalvm.js/js-scriptengine "20.0.0"]])
